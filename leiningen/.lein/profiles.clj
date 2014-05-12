8d72cfc6576de32926b180b2fb73c791559225ab
Chris Cornelison <chris.cornelison@pioneer.com>
2014-05-12 08:45:54 -0500

    update leiningen preferences

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-idefiles "0.2.0"]
-                  [cider/cider-nrepl "0.6.0"]]}}
-
+                  [cider/cider-nrepl "0.6.0"]
+                  [lein-create-template "0.1.1"]]}}
